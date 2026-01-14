21e3a5e224655b883131d74e171f7cce823db227
Pék Dominik <PekDominik@pg.local>
2026-01-14 11:32:17 +0100

    randomize countries

---
+++ Hangman.py
@@ -0,0 +1,23 @@
+import random
+
+
+from contry_and_capital_list import countries_and_capitals
+
+
+country=[]
+capital=[]
+
+for i in countries_and_capitals():
+    x, y = i.strip().split("|")
+    country.append(x)
+    capital.append(y)
+ran_country=random.randint(0, (len(capital)))
+print(country[ran_country])
+
+
+
+
+
+
+
+

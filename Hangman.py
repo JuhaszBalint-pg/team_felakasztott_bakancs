bb82d069648274a6ce44de02f56d9a29fa21541d
PekDominim <pek.dominik@diak.szbi-pg.hu>
2026-01-21 10:40:46 +0100

    haladas

--- Hangman.py
+++ Hangman.py
@@ -6,15 +6,17 @@ from contry_and_capital_list import countries_and_capitals
 
 country=[]
 capital=[]
-while True:
-    for i in countries_and_capitals():
-        x, y = i.strip().split("|")
-        country.append(x)
-        capital.append(y)
-    ran_country=random.randint(0, (len(country)))
-    ran_capital=random.randint(0, (len(capital)))
-    print(country[ran_country])
-    print(capital[ran_capital])
+
+for i in countries_and_capitals():
+    x, y = i.strip().split("|")
+    x=x.replace(" ", "")
+    y=y.replace(" ", "")
+    country.append(x)
+    capital.append(y)
+ran_country=random.randint(0, (len(country)))
+ran_capital=random.randint(0, (len(capital)))
+print(country[ran_country])
+print(capital[ran_capital])
 
 
 

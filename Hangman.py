41019718050b47ecdc112fd4fb3dea3d124ad984
Rónai Márton <RonaiMarton@pg.local>
2026-01-21 11:34:11 +0100

    semii nem müködik (egyenlőre)

--- Hangman.py
+++ Hangman.py
@@ -2,25 +2,7 @@ import random
 
 
 from contry_and_capital_list import countries_and_capitals
-
-hidden1 = []
-hidden2 = []
-country=[]
-capital=[]
-
-for i in countries_and_capitals():
-    x, y = i.strip().split("|")
-    x=x.lstrip()
-    y=y.lstrip()
-    country.append(x)
-    capital.append(y)
-ran_country=random.randint(0, (len(country)))
-ran_capital=random.randint(0, (len(capital)))
-
-
-city = capital[ran_capital]
-country = country[ran_country]
-
+from ascii import HANGMANPICS
 
 def hangmanci(city):
     for i in city:
@@ -38,15 +20,40 @@ def hangmancu(country):
         else:
             hidden2.append('-')
     print(''.join(hidden2))
-    return country, country.count(' ')   
+    return country, country.count(' ')
+
+fut = True
+hidden1 = []
+hidden2 = []
+orszag=[]
+capital=[]
+
+while fut:
+    for i in countries_and_capitals():
+        x, y = i.strip().split("|")
+        x=x.lstrip()
+        y=y.lstrip()
+        orszag.append(x)
+        capital.append(y)
+    ran_country=random.randint(0, (len(orszag)))
+    ran_capital=random.randint(0, (len(capital)))
 
 
-hangmanci(city)
-hangmancu(country)
+    city = capital[ran_capital]
+    country = orszag[ran_country]
 
+    print("[1] Ország")
+    print("[2] Főváros")
+    print("[ENTER] Kilépés")
+    choice = input()
 
+    if choice == '1':
+        hangmancu(country)
+        country.clear()
+    elif choice == '2':
+        hangmanci(city)
+    elif choice == '':
+        Fut = False
 
-
-
-
-
+hibapont = 0
+print('Köszönjük a játékot')

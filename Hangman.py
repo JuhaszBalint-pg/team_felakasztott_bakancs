5c9669cd706b4c3ffc48c9ede854e61905d8fd74
Juhász Bálint <JuhaszBalint@pg.local>
2026-01-28 11:22:47 +0100

    kabbe gecu

--- Hangman.py
+++ Hangman.py
@@ -22,28 +22,35 @@ def hangmancu(country):
     print(''.join(hidden2))
     return country, country.count(' ')
 
+def reveal():
+    
+
 kitalalas = True
 fut = True
 hibapont = 0
-hibasbetu = []
+hibas = []
 hidden1 = []
 hidden2 = []
 orszag=[]
 capital=[]
 
-while fut:
-    for i in countries_and_capitals():
+
+for i in countries_and_capitals():
         x, y = i.strip().split("|")
         x=x.lstrip()
         y=y.lstrip()
         orszag.append(x)
         capital.append(y)
-    ran_country=random.randint(0, (len(orszag)))
-    ran_capital=random.randint(0, (len(capital)))
+ran_country= random.randint(0, (len(orszag)))
+ran_capital= random.randint(0, (len(capital)))
 
 
+while fut==True:
     city = capital[ran_capital]
     country = orszag[ran_country]
+    cityconstant = city
+    countryconstant = country
+
 
     print('Mit szeretnél kitalálni?')
     print("[1] Ország")
@@ -55,18 +62,19 @@ while fut:
         hangmancu(country)
     elif choice == '2':
         hangmanci(city)
-    elif choice == '':
-        Fut = False
-'''
-while kitalalas:
-    guess = input('Adj meg 1db betűt!')
-    if guess in country or guess in city:
-        
-    elif:
-        city.append(guess)
-        country.append(guess)
-        
-    if hibapont == len(country) or hibapont == len(city)
+    else:
+        fut=False
+
+    while kitalalas:
+        guess = input('Adj meg 1db betűt!')
+        if guess in country:
+            
+        elif guess not in country:
+            hibas.append(guess)
+            print(f'{hibas}')
+            
+        hibapont == len(country) or hibapont == len(city)
         kitalalas = False
-'''
+
 print('Köszönjük a játékot')
+print(capital[ran_capital])
